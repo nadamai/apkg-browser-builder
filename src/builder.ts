5f1eea30c1bce81779a3081877b703482335ac64
Daniel Stasiak <dan@digitibox.pl>
2024-08-11 18:25:50 +0200

    feat: added file-saver for saving files in browser

--- src/builder.ts
+++ src/builder.ts
@@ -1,10 +1,10 @@
-import { File } from './file.js';
+import * as FileSaver from 'file-saver';
 
 const ApkgBuilder = {
 	init(): void {
 		const uri = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
 
-		File.save('file.gif', uri);
+		FileSaver.saveAs(uri, 'apkg.gif');
 	}
 };
 
