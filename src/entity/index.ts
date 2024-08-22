10f01ba625239614b240d055431d1fbaf646cff9
Daniel Stasiak <dan@digitibox.pl>
2024-08-22 23:38:35 +0200

    fix: removed unresolved deck

--- src/entity/index.ts
+++ src/entity/index.ts
@@ -1,6 +1,5 @@
 export { Card } from './card';
 export { Collection } from './collection';
-export { Deck } from './deck';
 export { Grave } from './grave';
 export { Note } from './note';
 export { ReviewLog } from './review-log';
