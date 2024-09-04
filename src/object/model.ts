94411b706274fed00de845b707d31747d4e28d87
Daniel Stasiak <dan@digitibox.pl>
2024-09-04 21:03:45 +0200

    feat: model default data for objects

--- src/object/model.ts
+++ src/object/model.ts
@@ -1,9 +1,31 @@
 import { Object } from '../abstract';
+import { ModelType } from '../dictionary/model-type';
 import { Model as ModelObject } from '../model';
 
 export class Model extends Object<ModelObject> {
 	protected object: ModelObject = {
-		id: 1
+		css: '',
+		did: 1,
+		flds: [],
+		id: 1,
+		latexPre:
+			'\\documentclass[12pt]{article}' +
+			'\\special{papersize=3in,5in}' +
+			'\\usepackage[utf8]{inputenc}' +
+			'\\usepackage{amssymb,amsmath}' +
+			'\\pagestyle{empty}' +
+			'\\setlength{\\parindent}{0in}' +
+			'\\begin{document}',
+		latexPost: '\\end{document}',
+		mod: 0,
+		name: '',
+		req: [],
+		sortf: 0,
+		tags: [],
+		tmpls: [],
+		type: ModelType.standard,
+		usn: 0,
+		vers: []
 	};
 
 	public getId(): number {
