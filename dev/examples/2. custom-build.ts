d3218f01904bbb84efec15f11e4debf44a3eba43
Daniel Stasiak <daniel@digitibox.pl>
2026-07-12 21:37:14 +0200

    feat: custom-build dev example

---
+++ dev/examples/2. custom-build.ts
@@ -0,0 +1,32 @@
+import ApkgBuilder, { Card, Collection, Deck } from 'apkg-browser-builder';
+
+/**
+ * This is the example of how to create an .apkg file and use it as a Blob.
+ *
+ * The collection object is the main object contained within the .apkg file.
+ * When the card is created it can be added to the deck which is linked to the collection.
+ */
+
+// Creating a new collection
+const collection = new Collection();
+
+// Creating a deck
+const deck = new Deck('My deck');
+
+// Adding the deck to the collection
+collection.addDeck(deck);
+
+// Creating a card with the initial note data
+const card = new Card('This is front', 'This is back');
+
+// Adding the card to the deck
+deck.addCard(card);
+
+// Building and saving the .apkg file
+const apkg = new ApkgBuilder(collection);
+
+await apkg.init();
+
+const blob = await apkg.build();
+
+console.log(blob);
