0a1ea6353b688caa944320f525c3fb0695e41c2e
Daniel Stasiak <daniel@digitibox.pl>
2026-07-14 11:10:23 +0200

    style: enable prettier for typedoc comments

--- src/type/apkg-builder-config.ts
+++ src/type/apkg-builder-config.ts
@@ -1,19 +1,19 @@
 import { SqlJsConfig } from 'sql.js';
 
-/**
- * Optional configuration of {@link ApkgBuilder}.
- */
+/** Optional configuration of {@link ApkgBuilder}. */
 export type ApkgBuilderConfig = {
 	/**
-	 * Options forwarded to [`initSqlJs()`](https://sql.js.org/documentation/global.html#initSqlJs) of sql.js, which loads the WebAssembly build of SQLite used to create the ANKI database when {@link ApkgBuilder} `build` is called.
+	 * Options forwarded to [`initSqlJs()`](https://sql.js.org/documentation/global.html#initSqlJs) of sql.js, which loads the WebAssembly
+	 * build of SQLite used to create the ANKI database when {@link ApkgBuilder} `build` is called.
 	 *
-	 * By default, the `sql-wasm-browser.wasm` binary bundled with this package is used. Provide a `locateFile` callback to load the `.wasm` file from somewhere else instead, such as a CDN or your own static assets:
+	 * By default, the `sql-wasm-browser.wasm` binary bundled with this package is used. Provide a `locateFile` callback to load the `.wasm`
+	 * file from somewhere else instead, such as a CDN or your own static assets:
 	 *
 	 * ```ts
 	 * const builder = new ApkgBuilder(collection, {
-	 *     sqljs: {
-	 *         locateFile: (file) => `https://sql.js.org/dist/${file}`
-	 *     }
+	 * 	sqljs: {
+	 * 		locateFile: (file) => `https://sql.js.org/dist/${file}`
+	 * 	}
 	 * });
 	 * ```
 	 */
